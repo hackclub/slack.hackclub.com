b5bdff5f51f39ba9f011ed867d10b3871d2fde2f
Eternal <chasezou09@gmail.com>
2026-07-02 14:14:20 -0400

    oh we use turbopack now

--- next.config.mjs
+++ next.config.mjs
@@ -12,12 +12,13 @@ const nextConfig = {
     ]
   },
 
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    })
-    return config
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   },
 }
 
